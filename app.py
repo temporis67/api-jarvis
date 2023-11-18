9e1e4bdc30d64206661db1803c8541978b2aed4c
temporis67 <ahem00@gmail.com>
2023-11-18 18:27:01 +0100

    delete & update question

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 # project specific
 from db.db_tool import DB
 from definitions.user import User
+from definitions.question import Question
 from pprint import pprint
 
 app = Flask(__name__)
@@ -38,34 +39,32 @@ def fetch_user():
 
     try:
         if request.method == 'POST':
-            print("fetch_user() POST method")
+            # print("fetch_user() POST method")
             result = request.form
             for key in user.__dict__:
                 if key in result and result[key] is not None and result[key] != '':
                     setattr(user, key, result[key])
-                    print("fetch_user() %s : %s" % (key, result[key]))
+                    # print("fetch_user() %s : %s" % (key, result[key]))
                 else:
-                    print("fetch_user() %s not found" % key)
+                    # print("fetch_user() %s not found" % key)
+                    pass
         else:
             print("request not used the POST method")
     except Exception as e:
         print("Error bei app.fetch_user(): %s " % e)
 
-    # msg = {'name': name, 'password': password, 'email': email}
-    # user = my_api.login_user(msg=msg)
-    user3 = my_db.get_user(user=user)
+    user = my_db.get_user(user=user)
 
-    print("HERE USER3 fetch_user() Ende:")
-    pprint(vars(user3))
-    user2 = {'name': 'Peter', 'uuid': 'c382bc64-7817-11ee-b70f-047c16bbac51', 'email': 'ahem00@gmail.com', 'password': '123456'}
-    return user2
+    print("fetch_user() Success: ")
+    pprint(vars(user))
 
+    return user.__dict__, 200
 
-#
 
 @app.route('/api/questions', methods=['POST', 'GET'])
 @cross_origin()
 def fetch_questions():
+    print("app.fetch_questions() Start")
     if request.method != 'POST':
         return jsonify({'error': 'Only POST method is allowed'}), 405
 
@@ -75,9 +74,99 @@ def fetch_questions():
 
     try:
         questions = my_db.get_questions(user_uuid=user_uuid)
+        print("app.fetch_questions() Success - %s questions found" % len(questions))
         return jsonify(questions), 200
     except Exception as e:
         print("ERROR:: app.fetch_questions(): %s" % e)
+        # Hier ein geeignetes Logging-Framework verwenden
+        return jsonify({'error': 'Internal server error'}), 500
+
+@app.route('/api/new_question', methods=['POST', 'GET'])
+@cross_origin()
+def new_question():
+    if request.method != 'POST':
+        return jsonify({'error': 'Only POST method is allowed'}), 405
+
+    user_uuid = request.form.get('user_uuid')
+    if not user_uuid:
+        print("ERROR:: app.new_question(): No user_uuid provided")
+        return jsonify({'error': 'No user_uuid provided'}), 400
+
+    title = request.form.get('title')
+    if not title:
+        print("ERROR:: app.new_question(): No title provided")
+        return jsonify({'error': 'No title provided'}), 400
+
+    content = request.form.get('content')
+    if not content:
+        print("ERROR:: app.new_question(): No content provided")
+        return jsonify({'error': 'No content provided'}), 400
+
+    try:
+        question = my_db.new_question(user_uuid=user_uuid, title=title, content=content)
+        return jsonify(question), 200
+    except Exception as e:
+        print("ERROR:: app.new_question(): %s" % e)
+        # Hier ein geeignetes Logging-Framework verwenden
+        return jsonify({'error': 'Internal server error'}), 500
+
+
+@app.route('/api/update_question', methods=['POST', 'GET'])
+@cross_origin()
+def update_question():
+    if request.method != 'POST':
+        return jsonify({'error': 'Only POST method is allowed'}), 405
+
+    user_uuid = request.form.get('user_uuid')
+    if not user_uuid:
+        print("ERROR:: app.update_question(): No user_uuid provided")
+        return jsonify({'error': 'No user_uuid provided'}), 400
+
+    question_uuid = request.form.get('question_uuid')
+    if not question_uuid:
+        print("ERROR:: app.update_question(): No question_uuid provided")
+        return jsonify({'error': 'No question_uuid provided'}), 400
+
+    title = request.form.get('title')
+    if not title:
+        print("ERROR:: app.update_question(): No title provided")
+        return jsonify({'error': 'No title provided'}), 400
+
+    content = request.form.get('content')
+    if not content:
+        print("ERROR:: app.update_question(): No content provided")
+        return jsonify({'error': 'No content provided'}), 400
+
+    try:
+        question = my_db.update_question(user_uuid=user_uuid, question_uuid=question_uuid, title=title, content=content)
+        return jsonify(question), 200
+    except Exception as e:
+        print("ERROR:: app.update_question(): %s" % e)
+        # Hier ein geeignetes Logging-Framework verwenden
+        return jsonify({'error': 'Internal server error'}), 500
+
+# TODO: Implement delete_question
+@app.route('/api/delete_question', methods=['POST', 'GET'])
+@cross_origin()
+def delete_question():
+    if request.method != 'POST':
+        return jsonify({'error': 'Only POST method is allowed'}), 405
+
+    user_uuid = request.form.get('user_uuid')
+    if not user_uuid:
+        print("ERROR:: app.delete_question(): No user_uuid provided")
+        return jsonify({'error': 'No user_uuid provided'}), 400
+
+    question_uuid = request.form.get('question_uuid')
+    if not question_uuid:
+        print("ERROR:: app.delete_question(): No question_uuid provided")
+        return jsonify({'error': 'No question_uuid provided'}), 400
+
+    try:
+        question = my_db.delete_question(user_uuid=user_uuid, question_uuid=question_uuid)
+        return jsonify(question), 200
+    except Exception as e:
+        print("ERROR:: app.delete_question(): %s" % e)
         # Hier ein geeignetes Logging-Framework verwenden
         return jsonify({'error': 'Internal server error'}), 500
 
--- db/db_tool.py
+++ db/db_tool.py
@@ -15,7 +15,7 @@ class DB:
     def get_questions(self, user_uuid=None):
         # Überprüfen, ob eine user_uuid vorhanden ist
         if user_uuid is None:
-            print("ERROR db_tool.get_questions(user_uuid):: No user_uuid given.")# Log error oder raise an exception
+            print("ERROR db_tool.get_questions(user_uuid):: No user_uuid given.")  # Log error oder raise an exception
             return {}
 
         # Parametrisierte Abfrage verwenden
@@ -33,7 +33,7 @@ class DB:
                 questions = {}
                 for qu in res:
                     questions[qu[0]] = {
-                        'user_uuid': qu[0],
+                        'uuid': qu[0],
                         'title': qu[1],
                         'content': qu[2],
                         'date_created': str(qu[3]),
@@ -95,3 +95,102 @@ class DB:
             database="ai-chat-db",
             user="ai-chat-pguser",
             password="--jarvis+")
+
+    def new_question(self, user_uuid, title, content):
+
+        # Überprüfen, ob eine user_uuid vorhanden ist
+        if user_uuid is None:
+            print("ERROR db_tool.new_question(user_uuid):: No user_uuid given.")
+            return {}
+
+        query = ("insert into questions (uuid, title,content) values (DEFAULT,%s,%s) RETURNING uuid, date_created, "
+                 "date_updated")
+        values = (title, content)
+
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(query, values)
+                res = cur.fetchone()
+
+                question = {
+                    'uuid': res[0],
+                    'title': title,
+                    'content': content,
+                    'date_created': str(res[1]),
+                    'date_updated': str(res[2])
+                }
+
+                print("New Question in DB: %s" % str(question))
+
+                update_user_query = ("insert into user_question (user_uuid, question_uuid) values (%s, %s)")
+                update_user_values = (user_uuid, question['uuid'])
+
+                cur.execute(update_user_query, update_user_values)
+
+                self.conn.commit()
+
+                return question
+
+        except Exception as e:
+            print("ERROR db_tool.new_question(user_uuid):: %s" % e)
+            return {}
+
+    def update_question(self, user_uuid, question_uuid, title, content):
+
+        # Überprüfen, ob eine user_uuid vorhanden ist
+        if user_uuid is None:
+            print("ERROR db_tool.update_question(user_uuid):: No user_uuid given.")
+            return {}
+
+        query = ("update questions set title = %s, content = %s, date_updated = DEFAULT where uuid = %s RETURNING "
+                 "date_updated")
+        values = (title, content, question_uuid)
+
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(query, values)
+                res = cur.fetchone()
+
+                question = {
+                    'uuid': question_uuid,
+                    'title': title,
+                    'content': content,
+                    'date_updated': str(res[0])
+                }
+
+                print("Updated Question in DB: %s" % str(question))
+
+                self.conn.commit()
+
+                return question
+
+        except Exception as e:
+            print("ERROR db_tool.update_question(user_uuid):: %s" % e)
+            return {}
+
+    def delete_question(self, user_uuid, question_uuid):
+
+        # Überprüfen, ob eine user_uuid vorhanden ist
+        if user_uuid is None:
+            print("ERROR db_tool.delete_question(user_uuid):: No user_uuid given.")
+            return {}
+
+        query = ("delete from questions where uuid = %s")
+        values = (question_uuid,)
+        query2 = ("delete from user_question where question_uuid = %s")
+        values2 = (question_uuid,)
+
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(query, values)
+                cur.execute(query2, values2)
+
+                print("Deleted Question in DB: %s" % question_uuid)
+
+                self.conn.commit()
+
+                return {}
+
+        except Exception as e:
+            print("ERROR db_tool.delete_question(user_uuid):: %s" % e)
+            return {}
